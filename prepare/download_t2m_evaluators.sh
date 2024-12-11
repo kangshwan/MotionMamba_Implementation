7f27e4e0d186cbe926239a9b6da352dc60d6f7e8
kangshwan <rnltls95@naver.com>
2024-12-11 18:41:49 +0900

    FIX: Minor adjustments in shell script and environment.yaml
    - Updated shell script to improve compatibility with deployment
    - Tweaked dependencies in environment.yaml to resolve conflicts

--- prepare/download_t2m_evaluators.sh
+++ prepare/download_t2m_evaluators.sh
@@ -7,7 +7,7 @@ echo "The t2m evaluators will be stored in the './deps' folder"
 echo "Downloading"
 gdown "https://drive.google.com/uc?id=1AYsmEG8I3fAAoraT4vau0GnesWBWyeT8"
 echo "Extracting"
-tar xfzv t2m.tar.gz
+tar --strip-components=1 -xzf t2m.tar.gz
 echo "Cleaning"
 rm t2m.tar.gz
 
